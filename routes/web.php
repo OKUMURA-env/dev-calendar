132ccfa6503674986e25829938234a42298b3d0d
OKUMURA-env <113576933+OKUMURA-env@users.noreply.github.com>
2023-06-17 08:22:34 +0900

    updateメソッドをルーティングに追加

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/calendar', [ScheduleController::class, 'calendar'])->name('calendar');
 Route::post('/schedule-get', [ScheduleController::class, 'scheduleGet'])->name('schedule-get');
 Route::post('/schedule-add', [ScheduleController::class, 'scheduleAdd'])->name('schedule-add');
+Route::post('/schedule-update', [ScheduleController::class, 'scheduleUpdate'])->name('schedule-update');
